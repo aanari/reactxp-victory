1bc10c3b645209276098f98cf5afbb5306c0d045
Ali Anari <ali@anari.io>
2017-11-26 17:33:47 -0800

    Proof-of-concept

---
+++ README.md
@@ -0,0 +1,11 @@
+# reactxp-victory
+This module provides cross-platform support for Victory (chart components) within the [ReactXP](https://microsoft.github.io/reactxp/) library.
+
+## Documentation
+Coming soon.
+
+### Prerequisites
+* [ReactXP](https://github.com/microsoft/reactxp/)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ src/web/PluginBase.tsx
@@ -0,0 +1,29 @@
+// import { VictoryTheme } from "victory-core";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryGroup,
+  VictoryStack,
+  VictoryBar,
+  VictoryLine,
+  VictoryArea,
+  VictoryScatter,
+  VictoryPie,
+  VictoryLabel,
+  VictoryLegend,
+} from "victory";
+// import Types = require('../common/Types');
+
+export {
+  VictoryAxis,
+  VictoryChart,
+  VictoryGroup,
+  VictoryStack,
+  VictoryBar,
+  VictoryLine,
+  VictoryArea,
+  VictoryScatter,
+  VictoryPie,
+  VictoryLabel,
+  VictoryLegend,
+};
